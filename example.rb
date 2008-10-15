80a8ceb452c76eb40e7103cec4f523c1976f27cb
Jacob Fugal <lukfugl@gmail.com>
2008-10-15 10:44:58 -0600

    getting started

---
+++ example.rb
@@ -0,0 +1,26 @@
+require 'hydra'
+
+module Resource
+  class Posts < Hydra::Resource
+    uri "/"
+  end
+
+  class Post < Hydra::Resource
+    uri "/:slug",
+      :slug => /[a-z-]+/i
+  end
+
+  class Comments < Hydra::Resource
+    uri "/:slug/comments",
+      :slug => /[a-z-]+/i
+  end
+
+  class Comment < Hydra::Resource
+    uri "/:slug/comments/:id",
+      :slug => /[a-z-]+/i,
+      :id => /\d+/
+  end
+end
+
+Hydra::Router.routes.compress
+Hydra::Router.routes.dump
